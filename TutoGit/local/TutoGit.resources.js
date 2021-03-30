328cd802f529dd7b96d8d183779c7f309db16cec
Rhrotsy <hosana.rotsy@gmail.com>
2021-03-30 18:00:25 +0200

    Ajout fichier gitscript.js

--- TutoGit/local/TutoGit.resources.js
+++ TutoGit/local/TutoGit.resources.js
@@ -5,3 +5,6 @@
 
 var POPUPS = POPUPS || ctx.addApplication('POPUPS');
 
+
+
+var GestionDuTemps = ctx.addApplication('GestionDuTemps', {"nature":"WEB3","path":"https://safeyrproject.com/user/60192035c4d5c5079c1d32db"});
